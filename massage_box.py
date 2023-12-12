74c5c181d8afcdf128920732e5c7e0ff9f29bf88
Saman <148078885+Samanghahremani@users.noreply.github.com>
2023-12-12 23:20:48 +0330

    massage_box.py

--- massage_box.py
+++ massage_box.py
@@ -5,10 +5,10 @@ window = Tk()
 window.title("massagebox")
 window.iconbitmap("image/box.ico")
 def callback():
-    lable = messagebox.askyesno("یه سوال","آیا تو بهترینی؟ ")
+    lable = messagebox.askyesno("a question?","are you the best?")
     if lable == 0:
         callback()
     elif lable == 1:
-        lable = messagebox.showinfo("آفرین","می دونستم")
+        lable = messagebox.showinfo("good job","I knew")
 button = Button(window,text="click here for continue",padx=100,pady=100,command=callback).grid()
 window.mainloop()
