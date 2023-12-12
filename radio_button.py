542041851b3fe84b133ec3895aa6a17b7e10a0a4
Saman <148078885+Samanghahremani@users.noreply.github.com>
2023-12-12 23:41:27 +0330

    Add files via upload

---
+++ radio_button.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.title("radio button")
+
+ra_val = IntVar()
+
+def av():
+    lable = messagebox.askyesno("Warning", "Are you sure?")
+    if lable == 1:
+        window2 = Toplevel()
+        lable2 = Label(window2,text="Thanks").pack()
+    elif lable == 0:
+        window2 = Toplevel()
+        lable2 = Label(window2,text="boro peye karet").pack()
+
+Radiobutton(window,value=1,text="pizza",variable=ra_val).pack(anchor=W)
+Radiobutton(window,value=2,text="sandwich",variable=ra_val).pack(anchor=W)
+Radiobutton(window,value=3,text="burger",variable=ra_val).pack(anchor=W)
+Radiobutton(window,value=4,text="pasta",variable=ra_val).pack(anchor=W)
+
+button = Button(window,text="done",command=av).pack()
+window.mainloop()
